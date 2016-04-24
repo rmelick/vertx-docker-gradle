4432501656c7309ef591c2842f5e98baaf296afc
rmelick <rmelick@cs.hmc.edu>
2016-04-24 21:24:01 +0200

    Working docker build with plugin

--- vertx/src/main/java/net/rmelick/vertxdocker/vertx/server/VertxServer.java
+++ vertx/src/main/java/net/rmelick/vertxdocker/vertx/server/VertxServer.java
@@ -1,5 +1,6 @@
 package net.rmelick.vertxdocker.vertx.server;
 
+import net.rmelick.vertxdocker.shared.ib.SharedLibraryClass;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.platform.Verticle;
@@ -8,12 +9,16 @@ import org.vertx.java.platform.Verticle;
  *
  */
 public class VertxServer extends Verticle  {
-    public void start() {
-        vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-            public void handle(HttpServerRequest req) {
-                String file = req.path().equals("/") ? "index.html" : req.path();
-                req.response().sendFile("webroot/" + file);
-            }
-        }).listen(8080);
-    }
+  public void start() {
+    vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+      public void handle(HttpServerRequest req) {
+        String localString = SharedLibraryClass.RMELICK_TEST_SHARED_STRING;
+        boolean successfullyLoaded = SharedLibraryClass.testSharedMethod() && SharedLibraryClass.testTransitiveDependency();
+
+
+        String file = req.path().equals("/") ? "index.html" : req.path();
+        req.response().sendFile("webroot/" + file);
+      }
+    }).listen(8080);
+  }
 }
