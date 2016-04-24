977c054f9c9bb79ebd77b128207f4219ec751a6f
Russell Melick <rmelick@Lohengrin.local>
2016-04-24 20:30:43 +0200

    Add initial working dependencies setup and vertx module

---
+++ vertx/src/main/java/net/rmelick/vertxdocker/vertx/server/VertxServer.java
@@ -0,0 +1,19 @@
+package net.rmelick.vertxdocker.vertx.server;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.platform.Verticle;
+
+/**
+ *
+ */
+public class VertxServer extends Verticle  {
+    public void start() {
+        vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+                String file = req.path().equals("/") ? "index.html" : req.path();
+                req.response().sendFile("webroot/" + file);
+            }
+        }).listen(8080);
+    }
+}
